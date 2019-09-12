f2aa125ddbea198d36e564df4d0aef9b5f66d0f7
Mohamed Ben <mohamed@ipfinder.io>
2019-09-12 21:03:33 +0100

    fix error

--- examples/domainby.go
+++ examples/domainby.go
@@ -1,12 +1,11 @@
 package main
 
-import {
+import (
 	"fmt"
-	"github.com/ipfinder-io/ip-finder-go"
-}
+)
 
 func main() {
-	ipfinder := NewIPFinder("YOUR_TOKEN_GOES_HERE", "") 
+	ipfinder := NewIPFinder("YOUR_TOKEN_GOES_HERE", "")
 	ip, err := ipfinder.GetDomainBy("DZ")
 
 	if err != nil {
--- examples/getDomain.go
+++ examples/getDomain.go
@@ -1,9 +1,8 @@
 package main
 
-import {
+import (
 	"fmt"
-	"github.com/ipfinder-io/ip-finder-go"
-}
+)
 
 func main() {
 	ipfinder := NewIPFinder("YOUR_TOKEN_GOES_HERE", "")
